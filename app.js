b226351bf4a69cc7ceea80712549f77b44c8041a
barsha119 <barsha119@users.noreply.github.com>
2021-07-15 18:34:41 +0530

    updated node js file

--- app.js
+++ app.js
@@ -96,6 +96,6 @@ app.post('/customer', function (req, res) {
 
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000 , function () {
     console.log("server started on port 3000");
 });
